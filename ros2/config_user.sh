b5762b34bebc49d2ebc80347a6b9328f1f85afcd
zhenpeng ge <zhenpeng.ge@qq.com>
2022-08-28 19:38:37 +0800

    update script

--- ros2/config_user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# update .bashrc
-echo "export PATH=/usr/NX/scripts/vgl:\$PATH" >> ~/.bashrc
-echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
-
-rosdep update
---
+++ ros2/startup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+## initialize environment
+if [ ! -f "/docker_config/init_flag" ]; then
+    # create user
+    groupadd -g $GID $USER
+    useradd --create-home --no-log-init -u $UID -g $GID $USER
+    usermod -aG sudo $USER
+    echo "$USER:$PASSWORD" | chpasswd
+    chsh -s /bin/bash $USER
+    # copy xfce4 config
+    mkdir /home/$USER/.config
+    cp -r /docker_config/xfce4 /home/$USER/.config
+    chown -R $UID:$GID /home/$USER/.config
+    # update user's .bashrc
+    echo "export PATH=/usr/NX/scripts/vgl:\$PATH" >> /home/$USER/.bashrc
+    echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> /home/$USER/.bashrc
+    # ros user config 
+    su $USER -c "rosdep update"
+    # custom init
+    bash /docker_config/init.sh
+    # update init flag
+    echo  "ok" > /docker_config/init_flag
+fi
+
+/usr/sbin/sshd
+/etc/init.d/dbus start
+/etc/NX/nxserver --startup
+tail -f /usr/NX/var/log/nxserver.log
